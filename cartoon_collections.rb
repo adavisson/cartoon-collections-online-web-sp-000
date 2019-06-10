8497828a15cf183e1f6c223a68a902e562dcbd7c
Andrew Davisson <akdavisson4@gmail.com>
2019-06-10 01:25:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,11 @@ def summon_captain_planet(calls)# code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  
+  
+  
 end
 
 def find_the_cheese# code an argument here
