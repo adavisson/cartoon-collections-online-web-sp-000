4f889a5e909fbf965df254209a0b4c47f6eb2dfb
Andrew Davisson <akdavisson4@gmail.com>
2019-06-10 01:28:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def long_planeteer_calls(calls)# code an argument here
   # Your code here
   
   calls.each do |call|
-    if call.length <= 4
+    if call.length > 4
       return true
     end
   end
